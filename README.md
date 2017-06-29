81994f7e8b9186df9da8ebb9e9f9948cc946f8e7
Jeff Reiner <theundeadghost@gmail.com>
2017-06-29 17:38:43 -0400

    :sparkles: Adding editorconfig, set up post for hubprint-server

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # hubprint
-
 > Print your HubSpot pages!
--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
 gulp.task('default', ['browser-sync'], function () {
-	gulp.watch('*.html', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
 });
 
 // Static server
 gulp.task('browser-sync', function() {
-	browserSync.init({
-		server: {
-			baseDir: "./"
-		}
-	});
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
